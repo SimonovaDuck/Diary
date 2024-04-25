f03550c4272a0eb9a95642470d2b7421e68bd106
simonovaduck <simonovaaliska@gmail.com>
2024-04-25 22:24:56 +0700

    registration is working!!!!

--- app/src/main/java/com/example/diary/registration_activity.java
+++ app/src/main/java/com/example/diary/registration_activity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,16 +23,13 @@ import com.google.firebase.firestore.FirebaseFirestore;
 public class registration_activity extends Activity {
 
 	private View _bg___registration;
-	private View rectangle_12;
-	private View rectangle_13;
-	private View rectangle_14;
 	private TextView reg;
 	private TextView quest;
 	public TextView ent;
-	private EditText usernameedt, emaildt, passwordedt;
-	private  String username, email, password;
-	private  FirebaseFirestore db;
-
+	private EditText usernamedt, emaildt, passworddt;
+	private String username, email, password;
+	private FirebaseFirestore db;
+	private Button button_reg;
 
 
 	@Override
@@ -42,48 +40,49 @@ public class registration_activity extends Activity {
 		db = FirebaseFirestore.getInstance();
 
 		_bg___registration = findViewById(R.id._bg___registration);
-		rectangle_12 = findViewById(R.id.rectangle_12);
-		rectangle_13 = findViewById(R.id.rectangle_13);
-		rectangle_14 = findViewById(R.id.rectangle_14);
 		reg = findViewById(R.id.reg);
 		quest = findViewById(R.id.quest);
-		EditText passwordedt = findViewById(R.id.rectangle_13);
-		EditText emaildt = findViewById(R.id.rectangle_14);
-		EditText usernameedt = findViewById(R.id.rectangle_12);
+		passworddt = findViewById(R.id.rectangle_13);
+		emaildt = findViewById(R.id.rectangle_14);
+		usernamedt = findViewById(R.id.rectangle_12);
 		ent = findViewById(R.id.ent);
+		button_reg = findViewById(R.id.registration_bt);
 
+		button_reg.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				username = usernamedt.getText().toString();
+				email = emaildt.getText().toString();
+				password = passworddt.getText().toString();
+				if (TextUtils.isEmpty(username)) {
+					usernamedt.setError("Пожалуйста введите имя");
+				} else if (TextUtils.isEmpty(email)) {
+					emaildt.setError("Пожалуйста введите email");
+				} else if (TextUtils.isEmpty(password)) {
+					passworddt.setError("Пожалуйста введите пароль");
+				} else {
+					addDataToFirestore(username, email, password);
+					Intent intent = new Intent (v.getContext(),enteryes_activity.class);
+					startActivity(intent);
+				}
+			}
 
+		});
 
 	}
 
-	public void onClickEnt(View view){
-		Intent intent = new Intent (this,enteryes_activity.class);
+
+	public void onClickEnt(View view) {
+		Intent intent = new Intent(this, enteryes_activity.class);
 		startActivity(intent);
 	}
 
-	public void onClickregistration(View view){
-
-		username = usernameedt.getText().toString();
-		email = emaildt.getText().toString();
-		password = passwordedt.getText().toString();
-		if (TextUtils.isEmpty(username)) {
-			usernameedt.setError("Пожалуйста введите имя");
-		} else if (TextUtils.isEmpty(email)) {
-			emaildt.setError("Пожалуйста введите email");
-		} else if (TextUtils.isEmpty(password)) {
-			passwordedt.setError("Пожалуйста введите пароль");
-		} else {
-			addDataToFirestore(username, email, password);
-		}
 
 
-//		Intent intent = new Intent (this,enteryes_activity.class);
-//		startActivity(intent);
-	}
-	private void addDataToFirestore(String username, String email, String password){
+	private void addDataToFirestore(String username, String email, String password) {
 		CollectionReference dbusers = db.collection("users");
 
-		users users = new users(username, email,password);
+		users users = new users(username, email, password);
 		dbusers.add(users).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
 			@Override
 			public void onSuccess(DocumentReference documentReference) {
@@ -96,6 +95,5 @@ public class registration_activity extends Activity {
 			}
 		});
 	}
-
-
 }
+
